1d046fc04377292e48fdaa700c36072d8b8d8f38
omariip <aboushaar.omar@gmail.com>
2023-08-08 04:50:13 -0400

    implemented aliasing for top-5-cheap tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,15 @@
 /* eslint-disable no-shadow */
 const Tour = require('./../models/tourModel');
 
+// middle ware
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields =
+    'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // console.log(req.query);
