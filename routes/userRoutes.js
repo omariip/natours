af007e120e5ca220de0fcf02074ec4ffe1a82abe
omariip <aboushaar.omar@gmail.com>
2023-08-14 17:27:11 -0400

    implemented changing password for currently signed in user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,12 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
+
 router.post(
   '/forgotPassword',
   authController.forgotPassword
