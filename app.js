cb3dffbfd5478e56934249b295c45da4590a8325
omariip <aboushaar.omar@gmail.com>
2023-08-14 21:46:40 -0400

    added data sanitization against NoSQL query injection and XSS

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
-
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -10,11 +12,14 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) Glovbal MIDDLEWARES
+// set Security HTTP headers
+app.use(helmet());
 
 if (process.env.NODE_ENV === 'devlopment') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   // in one hour
@@ -25,10 +30,19 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
