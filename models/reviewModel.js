c3e9f0c12443bb8c6b90f28805988375ff01316f
omariip <aboushaar.omar@gmail.com>
2023-08-27 01:09:10 -0400

    implemented calculating the number of reviews and average ratings on a tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -49,6 +50,59 @@ reviewSchema.pre(/^find/, function(next) {
   });
 
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(
+  tourId
+) {
+  // console.log(tourId);
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  // console.log(stats);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5
+    });
+  }
+};
+
+reviewSchema.post('save', function() {
+  //this points to the current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+// ** This is one way of solving using pre and post which is longer but it shows how to send data between pre and post
+// reviewSchema.pre(/^findOneAnd/, async function(next) {
+//   this.r = await this.findOne();
+//   next();
+// });
+// reviewSchema.post(/^findOneAnd/, async function() {
+//   // this.findOne(); does not work here because query has already executed
+//   await this.r.constructor.calcAverageRatings(this.r.tour);
+// });
+
+// ** This is an easier way of calling the function with only post
+reviewSchema.post(/^findOneAnd/, async function(doc) {
+  if (doc) {
+    await doc.constructor.calcAverageRating(doc.tour);
+  }
 });
 
 const Review = mongoose.model('Review', reviewSchema);
